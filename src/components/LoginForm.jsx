d14201be97fb8582fdced6472a08edf7b9849475
yohann0011 <91480849+Yohann0011@users.noreply.github.com>
2026-02-18 16:19:08 -0500

    Update LoginForm.jsx

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -24,12 +24,6 @@ const LoginForm = () => {
 
   return (
     <div className="login-container">
-      {/* Icono de Usuario (SVG) */}
-      <div className="profile-icon">
-        <svg viewBox="0 0 24 24">
-          <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/>
-        </svg>
-      </div>
 
       <form onSubmit={handleSubmit}>
         
@@ -88,24 +82,10 @@ const LoginForm = () => {
         {/* Botón Ingresar */}
         <button type="submit" className="btn-signin">INGRESAR</button>
 
-        {/* Opciones extra */}
-        <div className="options">
-          <label>
-            <input type="checkbox" /> Remember me
-          </label>
-          <a href="#" className="forgot-pass">Forgot password?</a>
-        </div>
-
-        {/* Footer */}
-        <div className="footer">
-          Not a member?
-          <br />
-          <a href="#" className="btn-create">Create account</a>
-        </div>
-
       </form>
     </div>
   );
 };
 
 export default LoginForm;
+
